4421a298b401f1ec50012a6e15473f8653998f85
PrashamDesai <150163406+prasham9114@users.noreply.github.com>
2026-05-24 04:10:39 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: false,
+  },
 };
 
 export default nextConfig;
