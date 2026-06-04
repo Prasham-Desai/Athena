d6940a64997bdd40500b0e888d898137fbed2814
PrashamDesai <150163406+prasham9114@users.noreply.github.com>
2026-06-04 20:19:29 +0530

    Error Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -477,7 +477,7 @@ export default function DashboardPage() {
             </div>
 
             <div className="h-[220px] -mx-2">
-              <ResponsiveContainer width="100%" height="100%" minWidth={0}>
+              <ResponsiveContainer width="100%" height="100%" minWidth={0} minHeight={0}>
                 <AreaChart data={chartData} margin={{ top: 5, right: 10, left: -20, bottom: 0 }}>
                   <defs>
                     <linearGradient id="chartGradient" x1="0" y1="0" x2="0" y2="1">
